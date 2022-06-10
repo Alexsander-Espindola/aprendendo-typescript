487e53bef3c13ee09009ee174d23b5a4c913cc49
Alexsander Espindola <alexsander81002745@gmail.com>
2022-06-10 19:05:19 -0300

    Inferência de tipo

---
+++ src/introducao/type-annotation.ts
@@ -0,0 +1,27 @@
+// Redundância de inserção de tipo:
+// let name: string = 'Alexsander';
+// ts já iria atribuir a função um tipo string
+
+// Array
+const arrayOfNumbers: Array<number> = [1, 2, 3];
+const otherArrayOfnumbers: number[] = [4, 5, 6, 7];
+console.log(arrayOfNumbers, otherArrayOfnumbers);
+
+// Objetos
+const car: {
+  color: string;
+  year: number;
+  optional?: boolean;
+} = {
+  color: 'Blue',
+  year: 2004,
+};
+
+console.log(car);
+
+// Funções
+function sum(x: number, y: number): number {
+  return x + y;
+}
+const sum2: (x: number, y: number) => number = (x, y) => x + y;
+console.log(sum(10, 20), sum2(10, 25));
