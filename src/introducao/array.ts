d46c5a604bf51e4ff15f373db8697b9d1b3c4bc7
Alexsander Espindola <alexsander81002745@gmail.com>
2022-06-14 15:02:11 -0300

    Anotações sobre ReadonlyArray e tuplas

--- src/introducao/array.ts
+++ src/introducao/array.ts
@@ -2,3 +2,17 @@
 const arrayOfNumbers: Array<number> = [1, 2, 3];
 const otherArrayOfnumbers: number[] = [4, 5, 6, 7];
 console.log(arrayOfNumbers, otherArrayOfnumbers);
+
+function arrayFunc(...args: Array<number>): number {
+  return args.reduce((acc, value) => acc * value, 1);
+}
+
+// ReadOnly array
+const array: ReadonlyArray<string> = ['1', '2'];
+console.log(array);
+
+// Tuple
+const client: readonly [string?, ...number[]] = ['Alexsander', 22];
+
+console.log(arrayFunc(2, 3, 10));
+console.log(client, client[0]);
